0a19673390efaba4264785c3c95f8f4758cd4b2d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 14:59:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 require 'pry'
 def square_array(array)
-  # your code here
+  # your code here\
+   new_array = []
   array.each do |arr|
-   new_square = arr**2
-    puts new_square
+   new_square << arr**2
+    
     binding.pry
   end
+    new_square
 end
